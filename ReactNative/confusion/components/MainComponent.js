93d82f2bc9b193c94affe59d374eba23d588504b
= <=>
2020-08-06 20:10:25 +0530

    Navigation components2

--- ReactNative/confusion/components/MainComponent.js
+++ ReactNative/confusion/components/MainComponent.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import { View, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Icon } from 'react-native-elements';
+import { StatusBar } from 'react-native';
+
+const HomeNavigator = createStackNavigator()
+
+function HomeNavigatorScreen() {
+    return(
+        <HomeNavigator.Navigator
+            initialRouteName='Home'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    color: "#fff",        
+                },
+            }}
+        >
+            <HomeNavigator.Screen
+                name="Home"
+                component={Home}
+            />       
+        </HomeNavigator.Navigator>
+    );
+}
 
 const MenuNavigator = createStackNavigator();
 
 function MenuNavigatorScreen() {
     return(
         <MenuNavigator.Navigator
-            initialRouteName='Menu'
             screenOptions={{
                 headerStyle: {
                     backgroundColor: "#512DA8"
@@ -34,16 +61,38 @@ function MenuNavigatorScreen() {
     );
 }
 
+const MainNavigator = createDrawerNavigator()
+
+function MainNavigatorScreen() {
+    return(
+        <MainNavigator.Navigator drawerStyle={{backgroundColor: "#D1C4E9"}}>
+            <MainNavigator.Screen
+                name="Home"
+                component={HomeNavigatorScreen}
+            />  
+            <MainNavigator.Screen
+                name="Menu"
+                component={MenuNavigatorScreen}
+            />       
+        </MainNavigator.Navigator>
+    );
+}
+
+
 class Main extends Component {
 
-  render() {
- 
-    return (
-        <NavigationContainer>
-            <MenuNavigatorScreen/>           
-        </NavigationContainer>
-    );
-  }
+    componentDidMount() {
+        StatusBar.setHidden(true);
+    }  
+
+    render() {
+    
+        return (
+            <NavigationContainer >
+                <MainNavigatorScreen />     
+            </NavigationContainer>
+        );
+    }
 }
   
 export default Main;
