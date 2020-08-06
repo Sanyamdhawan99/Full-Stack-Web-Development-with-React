efc10b9ec51323d669ee9fdd96a06ddde8a759a5
= <=>
2020-08-06 18:29:39 +0530

    React Native Navigation part 1

--- ReactNative/confusion/components/MainComponent.js
+++ ReactNative/confusion/components/MainComponent.js
@@ -1,31 +1,49 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
-import { View } from 'react-native';
-import { DISHES } from '../shared/dishes';
+import { View, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-class Main extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        };
-    }
+const MenuNavigator = createStackNavigator();
 
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId})
-    }
-
-    render() {
-        return (
-            <View>
-                <Menu dishes={this.state.dishes} onPress={ (dishId) => this.onDishSelect(dishId) } />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-            </View>
-        );
-    }
+function MenuNavigatorScreen() {
+    return(
+        <MenuNavigator.Navigator
+            initialRouteName='Menu'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    color: "#fff"            
+                }
+            }}
+        >
+            <MenuNavigator.Screen
+                name="Menu"
+                component={Menu}
+            />
+            <MenuNavigator.Screen
+                name="DishDetail"
+                component={DishDetail}
+                options={{ headerTitle: "Dish Details"}}
+            />            
+        </MenuNavigator.Navigator>
+    );
 }
 
+class Main extends Component {
+
+  render() {
+ 
+    return (
+        <NavigationContainer>
+            <MenuNavigatorScreen/>           
+        </NavigationContainer>
+    );
+  }
+}
+  
 export default Main;
