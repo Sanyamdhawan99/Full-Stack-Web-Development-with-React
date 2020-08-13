3d2caee1aad5d1332347dbf4b28511216990bb3e
= <=>
2020-08-13 15:38:14 +0530

    sending email

--- ReactNative/confusion/components/ReservationComponent.js
+++ ReactNative/confusion/components/ReservationComponent.js
@@ -4,6 +4,8 @@ import { Icon } from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Moment from 'moment';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 class Reservation extends Component {
     constructor(props) {
@@ -34,7 +36,8 @@ class Reservation extends Component {
                     onPress: () => this.resetForm()
                 },
                 {
-                    text: 'Ok'
+                    text: 'Ok',
+                    onPress: () => {this.presentLocalNotification(this.state.date)}
                 }
             ],
             {cancelable: false}
@@ -49,6 +52,33 @@ class Reservation extends Component {
             show: false,
             mode: 'date'
         }); 
+    }
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if(permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notification');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for ' + date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        })
     }
 
     render() {
